ff2e13ad9895c590b65e1e69e7f4f29e3054259e
Görkem Dağlı <gorkem.dagli99@gmail.com>
2026-06-19 02:16:21 +0300

    chore: remove pre-commit test line

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,9 @@
 import { HelloTaskFlow } from './components/HelloTaskFlow';
 
 function App() {
-  const value = 'unused-string-but-used';
   return (
     <div className="min-h-screen bg-background text-foreground">
-      <HelloTaskFlow data-testid={value} />
+      <HelloTaskFlow />
     </div>
   );
 }
