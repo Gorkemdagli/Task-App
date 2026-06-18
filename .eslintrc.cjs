9b859d59d618e03dcbbc75d026dfd811c65c21d0
Görkem Dağlı <gorkem.dagli99@gmail.com>
2026-06-19 02:18:37 +0300

    chore: eslint + prettier + husky pre-commit chain

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: { browser: true, node: true, es2022: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 2022, sourceType: 'module' },
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['dist', 'build', 'node_modules', 'coverage', '*.config.js', '*.config.ts'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/consistent-type-imports': 'error',
+  },
+};
